ac483cf9a828cdd53712d4db93f7ca2b166237bc
AliGuillen <aligguillen@gmail.com>
2018-01-22 16:56:46 -0600

    multiple fixes

--- gpayments-connector-woocommerce.php
+++ gpayments-connector-woocommerce.php
@@ -46,8 +46,8 @@ class WC_GPayments_Connection extends WC_Payment_Gateway_CC {
 	public function init_form_fields() {
 		$this->form_fields = array(
 			'enabled' => array(
-				'title'		=> __( 'Enable / Disable', 'wc-4gpayments' ),
-				'label'		=> __( 'Enable this payment gateway', 'wc-4gpayments' ),
+				'title'		=> __( 'Activo / Inactivo', 'wc-4gpayments' ),
+				'label'		=> __( 'Activar esta forma de pago', 'wc-4gpayments' ),
 				'type'		=> 'checkbox',
 				'default'	=> 'no',
 			),
@@ -55,34 +55,54 @@ class WC_GPayments_Connection extends WC_Payment_Gateway_CC {
 				'title'		=> __( 'Title', '4gpayments' ),
 				'type'		=> 'text',
 				'desc_tip'	=> __( 'Payment title of checkout process.', 'wc-4gpayments' ),
-				'default'	=> __( 'Credit / Debit card', '4gpayments' ),
+				'default'	=> __( 'Pagar con tarjeta', '4gpayments' ),
+				'custom_attributes' => array(
+					'required' => 'required'
+				),
 			),
 			'description' => array(
 				'title'		=> __( 'Description', '4gpayments' ),
 				'type'		=> 'text',
 				'default'	=> __( 'Pagar con tu tarjeta de débito o crédito.', 'wc-4gpayments' ),
-				'css'		=> 'max-width:450px;'
+				'css'		=> 'max-width:450px;',
+				'custom_attributes' => array(
+					'required' => 'required'
+				),
 			),
 			'entity_description' => array(
-				'title'		=> __( 'Detalle bancario', 'wc-4gpayments' ),
+				'title'		=> __( 'Detalle bancario (max 22 caracteres)', 'wc-4gpayments' ),
 				'type'		=> 'text',
-				'default' 	=> '4GP',
-				'desc_tip'	=> __( 'Descripcion para el estado de cuenta de la tarjeta del cliete.', '4gpayments' ),
+				'default' 	=> 'Pago a traves de 4GP',
+				'desc_tip'	=> __( 'Detalle que aparece en el Estado de Cuenta del cliente final', '4gpayments' ),
+				'custom_attributes' => array(
+					'required' => 'required',
+					'maxlength' => '22'
+				),
 			),
 			'charge_description' => array(
 				'title'		=> __( '4GP Descripcion del cargo', 'wc-4gpayments' ),
 				'type'		=> 'text',
-				'desc_tip'	=> __( 'Es la descripcion por defecto de un cargo a la tarjeta de tu cliente', '4gpayments' ),
+				'default' 	=> 'Compra en linea',
+				'desc_tip'	=> __( 'Es la descripcion por defecto de un cargo (compra) a la tarjeta de tu cliente', '4gpayments' ),
+				'custom_attributes' => array(
+					'required' => 'required'
+				),
 			),
 			'client_id' => array(
 				'title'		=> __( '4GP Client ID', 'wc-4gpayments' ),
 				'type'		=> 'text',
 				'desc_tip'	=> __( 'API Client ID provisto por 4Geeks Payments.', 'wc-4gpayments' ),
+				'custom_attributes' => array(
+					'required' => 'required'
+				),
 			),
 			'client_secret' => array(
 				'title'		=> __( '4GP Client Secret', 'wc-4gpayments' ),
 				'type'		=> 'password',
 				'desc_tip'	=> __( 'API Client Secret provisto por 4Geeks Payments.', 'wc-4gpayments' ),
+				'custom_attributes' => array(
+					'required' => 'required'
+				),
 			)
 		);
 	}
@@ -94,12 +114,12 @@ class WC_GPayments_Connection extends WC_Payment_Gateway_CC {
 		$customer_order = new WC_Order( $order_id );
 
 		//API Auth URL
-		$api_auth_url = 'https://api.payments.4geeks.io/authentication/token/';	
+		$api_auth_url = 'https://api.payments.4geeks.io/authentication/token/';
 
 		//API base URL
 		$api_url = 'https://api.payments.4geeks.io/v1/charges/simple/create/';
 
-		$data_to_send = array("grant_type" => "client_credentials", 
+		$data_to_send = array("grant_type" => "client_credentials",
 								"client_id" => $this->client_id,
 								"client_secret" => $this->client_secret );
 
@@ -114,19 +134,25 @@ class WC_GPayments_Connection extends WC_Payment_Gateway_CC {
 
 		$api_token = json_decode( wp_remote_retrieve_body($response_token), true)['access_token'];
 
+		if(!empty($_POST['wc-4gpayments-card-number']) && !empty($_POST['wc-4gpayments-card-cvc']) && !empty($_POST['wc-4gpayments-card-expiry'])){
+			// This is where the fun stuff begins
+			if($this->entity_description == ''){
+				$this->entity_description = 'Pago procesado por 4Geeks Payments';
+			}
+			$payload = array(
+				"amount"             	=> $customer_order->get_total(),
+				"description"           => $this->description,
+				"entity_description"    => $this->entity_description,
+				"currency"           	=> get_woocommerce_currency(),
+				"credit_card_number"    => str_replace( array(' ', '-' ), '', $_POST['wc-4gpayments-card-number'] ),
+				"credit_card_security_code_number" => str_replace( array(' ', '-' ), '', $_POST['wc-4gpayments-card-cvc'] ),
+				"exp_month" 			=> substr($_POST['wc-4gpayments-card-expiry'], 0, 2),
+				"exp_year" 				=> "20" . substr($_POST['wc-4gpayments-card-expiry'], -2),
 
-		// This is where the fun stuff begins
-		$payload = array(
-			"amount"             	=> $customer_order->get_total(),
-			"description"           => $this->description,
-			"entity_description"    => "4GP",
-			"currency"           	=> get_woocommerce_currency(),
-			"credit_card_number"    => str_replace( array(' ', '-' ), '', $_POST['wc-4gpayments-card-number'] ),
-			"credit_card_security_code_number" => str_replace( array(' ', '-' ), '', $_POST['wc-4gpayments-card-cvc'] ),
-			"exp_month" 			=> substr($_POST['wc-4gpayments-card-expiry'], 0, 2),
-			"exp_year" 				=> "20" . substr($_POST['wc-4gpayments-card-expiry'], -2),
-
-		);
+			);
+		}else{
+			throw new Exception( __( 'N&#250;mero de Tarjeta, Fecha de Expiraci&#243;n y CVC son requeridos', 'wc-4gpayments' ) );
+		}
 
 		// Send this payload to 4GP for processing
 		$response = wp_remote_post( $api_url, array(
@@ -137,6 +163,8 @@ class WC_GPayments_Connection extends WC_Payment_Gateway_CC {
 			'headers' => array('authorization' => 'bearer ' . $api_token, 'content-type' => 'application/json'),
 		 ) );
 
+		 $JsonResponse = json_decode($response['body']);
+		 $response_Detail = $JsonResponse ->{'detail'};
 
 		if ( is_wp_error( $response ) )
 			throw new Exception( __( 'Hubo un problema para comunicarse con el procesador de pagos...', 'wc-4gpayments' ) );
@@ -146,8 +174,6 @@ class WC_GPayments_Connection extends WC_Payment_Gateway_CC {
 
 		// get body response while get not error
 		$responde_code = wp_remote_retrieve_response_code( $response );
-
-
 		// 1 or 4 means the transaction was a success
 		if ( $responde_code == 201 ) {
 			// Payment successful
@@ -166,8 +192,8 @@ class WC_GPayments_Connection extends WC_Payment_Gateway_CC {
 			);
 		} else {
 			//transiction fail
-			wc_add_notice( 'Error al procesar el pago', 'error' );
-			$customer_order->add_order_note( 'Error: '. 'Error al procesar el pago' );
+			wc_add_notice( $response_Detail, 'error' );
+			$customer_order->add_order_note( 'Error: '. $response_Detail );
 		}
 
 	}
