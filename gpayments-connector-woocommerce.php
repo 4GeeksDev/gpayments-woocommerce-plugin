be1c9dd087acc25c7615b088d724e4f694209217
AliGuillen <aligguillen@gmail.com>
2018-01-25 16:32:50 -0600

    plan option setup

--- gpayments-connector-woocommerce.php
+++ gpayments-connector-woocommerce.php
@@ -123,7 +123,9 @@ class WC_GPayments_Connection extends WC_Payment_Gateway_CC {
 								"client_id" => $this->client_id,
 								"client_secret" => $this->client_secret );
 
-
+		if(empty($_POST['wc-4gpayments-card-number']) || empty($_POST['wc-4gpayments-card-cvc']) ||  empty($_POST['wc-4gpayments-card-expiry'])){
+			throw new Exception( __( 'N&#250;mero de Tarjeta, Fecha de Expiraci&#243;n y CVC son requeridos', 'wc-4gpayments' ) );
+		}
 		$response_token = wp_remote_post( $api_auth_url, array(
 				'method' => 'POST',
 				'timeout' => 90,
@@ -134,10 +136,9 @@ class WC_GPayments_Connection extends WC_Payment_Gateway_CC {
 
 		$api_token = json_decode( wp_remote_retrieve_body($response_token), true)['access_token'];
 
-		if(!empty($_POST['wc-4gpayments-card-number']) && !empty($_POST['wc-4gpayments-card-cvc']) && !empty($_POST['wc-4gpayments-card-expiry'])){
 			// This is where the fun stuff begins
 			if($this->entity_description == ''){
-				$this->entity_description = 'Pago procesado por 4Geeks Payments';
+				$this->entity_description = 'Pago a traves de 4GP';
 			}
 			$payload = array(
 				"amount"             	=> $customer_order->get_total(),
@@ -150,9 +151,6 @@ class WC_GPayments_Connection extends WC_Payment_Gateway_CC {
 				"exp_year" 				=> "20" . substr($_POST['wc-4gpayments-card-expiry'], -2),
 
 			);
-		}else{
-			throw new Exception( __( 'N&#250;mero de Tarjeta, Fecha de Expiraci&#243;n y CVC son requeridos', 'wc-4gpayments' ) );
-		}
 
 		// Send this payload to 4GP for processing
 		$response = wp_remote_post( $api_url, array(
@@ -195,7 +193,6 @@ class WC_GPayments_Connection extends WC_Payment_Gateway_CC {
 			wc_add_notice( $response_Detail, 'error' );
 			$customer_order->add_order_note( 'Error: '. $response_Detail );
 		}
-
 	}
 
 	// Validate fields
@@ -212,4 +209,166 @@ class WC_GPayments_Connection extends WC_Payment_Gateway_CC {
 	}
 
 }
+/************************************************************************************************************************************************
+************************************************************************************************************************************************/
+
+function register_simple_rental_product_type() {
+
+	class WC_Product_Simple_Rental extends WC_Product {
+		    public $WC_GPayments_Connection;
+			public function __construct( $product ) {
+
+			$this->product_type = 'simple_rental';
+			parent::__construct( $product );
+		}
+	}
+}
+add_action( 'plugins_loaded', 'register_simple_rental_product_type' );
+/**
+ * Add to product type drop down.
+ */
+function add_simple_rental_product( $types ){
+
+	// Key should be exactly the same as in the class
+	$types[ 'simple_rental' ] = __( 'Plan' );
+
+	return $types;
+
+}
+add_filter( 'product_type_selector', 'add_simple_rental_product' );
+
+
+/**
+ * Show pricing fields for simple_rental product.
+ */
+function simple_rental_custom_js() {
+
+	if ( 'product' != get_post_type() ) :
+		return;
+	endif;
+
+	?><script type='text/javascript'>
+		jQuery( document ).ready( function() {
+			jQuery( '.options_group.pricing' ).addClass( 'show_if_simple_rental' ).show();
+		});
+
+	</script><?php
+
+}
+add_action( 'admin_footer', 'simple_rental_custom_js' );
+
+
+/**
+ * Add a custom product tab.
+ */
+function custom_product_tabs( $tabs) {
+
+	$tabs['rental'] = array(
+		'label'		=> __( 'Planes', 'woocommerce' ),
+		'target'	=> 'rental_options',
+		'class'		=> array( 'show_if_simple_rental', 'show_if_variable_rental'  ),
+	);
+
+	return $tabs;
+
+}
+add_filter( 'woocommerce_product_data_tabs', 'custom_product_tabs' );
+
+
+/**
+ * Contents of the rental options product tab.
+ */
+function rental_options_product_tab_content() {
+
+	global $post;
+
+	$this->WC_GPayments_Connection = new WC_GPayments_Connection();
+
+	$this->WC_GPayments_Connection->init_form_fields();
+
+	echo var_dump($this->init_form_fields->client_secret);
+
+	die;
+
+	$data_to_send = array("grant_type" => "client_credentials",
+							"client_id" => $this->init_form_fields->client_id,
+							"client_secret" => $this->init_form_fields->client_secret );
+	$api_auth_url = 'https://api.payments.4geeks.io/authentication/token/';
+	$response_token = wp_remote_post( $api_auth_url, array(
+			'method' => 'POST',
+			'timeout' => 90,
+			'blocking' => true,
+			'headers' => array('content-type' => 'application/json'),
+			'body' => json_encode($data_to_send, true)
+		) );
+
+	$api_token = json_decode( wp_remote_retrieve_body($response_token), true)['access_token'];
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, "https://api.payments.4geeks.io/v1/plans/mine");
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+	$headers = [
+	    '-X GET',
+	    "-H authorization: bearer $api_token"
+	];
+
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+	$server_output = curl_exec ($ch);
+	curl_close ($ch);
+
+	/*foreach ($array as &$valor) {
+	    $valor = $valor * 2;
+	}*/
+	// $array ahora es array(2, 4, 6, 8)
+	//unset($valor);
+
+
+
+	?><div id='rental_options' class='panel woocommerce_options_panel'><?php
+		?><div class='options_group'><?php
+
+			woocommerce_wp_select( array(
+				'label' => __('Planes Consultados', 'woocommerce'),
+				'options' => array($server_output)
+				)
+			);
+		?></div>
+	</div><?php
+
+
+}
+add_action( 'woocommerce_product_data_panels', 'rental_options_product_tab_content' );
+
+
+/**
+ * Save the custom fields.
+ */
+function save_rental_option_field( $post_id ) {
+
+	$rental_option = isset( $_POST['_enable_renta_option'] ) ? 'yes' : 'no';
+	update_post_meta( $post_id, '_enable_renta_option', $rental_option );
+
+	if ( isset( $_POST['_text_input_y'] ) ) :
+		update_post_meta( $post_id, '_text_input_y', sanitize_text_field( $_POST['_text_input_y'] ) );
+	endif;
+
+}
+add_action( 'woocommerce_process_product_meta_simple_rental', 'save_rental_option_field'  );
+add_action( 'woocommerce_process_product_meta_variable_rental', 'save_rental_option_field'  );
+
+
+/**
+ * Hide Attributes data panel.
+ */
+function hide_attributes_data_panel( $tabs) {
+
+	$tabs['attribute']['class'][] = 'hide_if_simple_rental hide_if_variable_rental';
+
+	return $tabs;
+
+}
+add_filter( 'woocommerce_product_data_tabs', 'hide_attributes_data_panel' );
 ?>
