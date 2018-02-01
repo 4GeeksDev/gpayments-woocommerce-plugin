1bfb459c1df12d868cd89f41cfdfcd85695cf24e
AliGuillen <aligguillen@gmail.com>
2018-01-31 19:52:01 -0600

    Plans saving and request

--- gpayments-connector-woocommerce.php
+++ gpayments-connector-woocommerce.php
@@ -39,8 +39,10 @@ class WC_GPayments_Connection extends WC_Payment_Gateway_CC {
 		if ( is_admin() ) {
 			add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
 		}
+		if ( ! session_id() && is_user_logged_in() ) {
+			session_start();
+		}
 	} // Here is the  End __construct()
-
 
 	// administration fields for specific Gateway
 	public function init_form_fields() {
@@ -105,6 +107,21 @@ class WC_GPayments_Connection extends WC_Payment_Gateway_CC {
 				),
 			)
 		);
+		$nombre_archivo = "auth.txt";
+	    if(file_exists($nombre_archivo)){
+	        //$mensaje = "El Archivo $nombre_archivo se ha modificado";
+	    }else{
+	        //$mensaje = "El Archivo $nombre_archivo se ha creado";
+	    }
+
+	    if($archivo = fopen($nombre_archivo, "w")){
+	        if(fwrite($archivo, $_POST['woocommerce_wc-4gpayments_client_id']. " ".$_POST['woocommerce_wc-4gpayments_client_secret']. "\n")){
+	            //echo "Se ha ejecutado correctamente";
+	        }else{
+	            //echo "Ha habido un problema al crear el archivo";
+	        }
+         fclose($archivo);
+	    }
 	}
 
 	// Response handled for payment gateway
@@ -126,7 +143,6 @@ class WC_GPayments_Connection extends WC_Payment_Gateway_CC {
 		if(empty($_POST['wc-4gpayments-card-number']) || empty($_POST['wc-4gpayments-card-cvc']) || empty($_POST['wc-4gpayments-card-expiry'])){
 			throw new Exception( __( 'N&#250;mero de Tarjeta, Fecha de Expiraci&#243;n y CVC son requeridos', 'wc-4gpayments' ) );
 		}
-
 		$response_token = wp_remote_post( $api_auth_url, array(
 				'method' => 'POST',
 				'timeout' => 90,
@@ -143,8 +159,8 @@ class WC_GPayments_Connection extends WC_Payment_Gateway_CC {
 			}
 			$payload = array(
 				"amount"             	=> $customer_order->get_total(),
-				"description"           => $this->description,
-				"entity_description"    => $this->entity_description,
+				"description"           => $this->charge_description,
+				"entity_description"    => strtoupper($this->entity_description),
 				"currency"           	=> get_woocommerce_currency(),
 				"credit_card_number"    => str_replace( array(' ', '-' ), '', $_POST['wc-4gpayments-card-number'] ),
 				"credit_card_security_code_number" => str_replace( array(' ', '-' ), '', $_POST['wc-4gpayments-card-cvc'] ),
@@ -194,7 +210,6 @@ class WC_GPayments_Connection extends WC_Payment_Gateway_CC {
 			wc_add_notice( $response_Detail, 'error' );
 			$customer_order->add_order_note( 'Error: '. $response_Detail );
 		}
-
 	}
 
 	// Validate fields
@@ -211,4 +226,205 @@ class WC_GPayments_Connection extends WC_Payment_Gateway_CC {
 	}
 
 }
+/************************************************************************************************************************************************
+************************************************************************************************************************************************/
+
+function register_simple_rental_product_type() {
+
+	class WC_Product_Simple_Rental extends WC_Product {
+			public function __construct( $product ) {
+
+			$this->product_type = 'simple_rental';
+			parent::__construct( $product );
+		}
+	}
+}
+add_action( 'plugins_loaded', 'register_simple_rental_product_type' );
+
+function add_simple_rental_product( $types ){
+
+	// Key should be exactly the same as in the class
+	$types[ 'simple_rental' ] = __( 'Plan' );
+
+	return $types;
+
+}
+add_filter( 'product_type_selector', 'add_simple_rental_product' );
+
+
+function simple_rental_custom_js() {
+
+	if ( 'product' != get_post_type() ) :
+		return;
+	endif;
+
+	?><script type='text/javascript'>
+		jQuery( document ).ready( function() {
+			jQuery( '.options_group.pricing' ).addClass( 'show_if_simple_rental' ).show();
+		});
+
+	</script><?php
+
+}
+add_action( 'admin_footer', 'simple_rental_custom_js' );
+
+
+
+function custom_product_tabs( $tabs) {
+
+	$tabs['rental'] = array(
+		'label'		=> __( 'Planes', 'woocommerce' ),
+		'target'	=> 'rental_options',
+		'class'		=> array( 'show_if_simple_rental', 'show_if_variable_rental'),
+	);
+
+	return $tabs;
+
+}
+add_filter( 'woocommerce_product_data_tabs', 'custom_product_tabs' );
+
+
+
+function rental_options_product_tab_content() {
+
+	global $post, $woocommerce;
+
+	if (!$fp = fopen("auth.txt", "r")){
+		echo "The file can't be opened";
+	}
+	$file = "auth.txt";
+	$fp = fopen($file, "r");
+	$contents = fread($fp, filesize($file));
+	fclose($fp);
+
+	$credentials = explode(" ", $contents);
+
+	$Client_Aunt = $credentials[3];
+
+
+	// $api_auth_url = 'https://api.payments.4geeks.io/authentication/token/';
+    //
+	// $data_to_send = array("grant_type"=>"client_credentials",
+	// 					  "client_id" => "kGnxskOMNedrqj8IVma23Qk2DQGpjjpAUUPLr85W",
+	// 					  "client_secret" => "VbCaLax7xnhsBIfmkwic6ifSZ4FRiMRfwrrtdSKhw89f17LRdyeW7zYYKLwvj2VLRbQzH6q0F7mRejGJwBIckHa7W2PlJNN4QLqQcnCMYQkYj1DK4uUv0MVCbYhVeyTX"
+	// 				);
+    //
+	// $response_token = wp_remote_post( $api_auth_url, array(
+	// 		'method' => 'POST',
+	// 		'timeout' => 90,
+	// 		'blocking' => true,
+	// 		'headers' => array('content-type' => 'application/json'),
+	// 		'body' => json_encode($data_to_send, true)
+	// 	) );
+    //
+	// $api_token = json_decode( wp_remote_retrieve_body($response_token), true)['access_token'];
+    //
+	// $nombre_archivo = "auth.txt";
+	//  if(file_exists($nombre_archivo)){
+	// 	//$mensaje = "El Archivo $nombre_archivo se ha modificado";
+	// 	}else{
+	// 		//$mensaje = "El Archivo $nombre_archivo se ha creado";
+	// 	}
+    //
+	// if($archivo = fopen($nombre_archivo, "w")){
+	// 	if(fwrite($archivo, $Client_Id. " " . $Client_Secret . " $api_token " . . "\n")){
+	// 		//echo "Se ha ejecutado correctamente";
+	// 	}else{
+	// 		//echo "Ha habido un problema al crear el archivo";
+	// 	}
+	// 	 fclose($archivo);
+
+	$body = array(
+		'X GET',
+		"-H authorization: bearer  $Client_Aunt"
+	);
+
+	$api_plan_url = 'https://api.payments.4geeks.io/v1/plans/mine';
+
+	$response_plan = wp_remote_post( $api_plan_url,
+						array(
+							'method' => 'GET',
+							'timeout' => 90,
+							'blocking' => true,
+							'headers' => array('content-type' => 'application/json'),
+							'body' => json_encode($body, true)
+						)
+								  );
+
+  $planes =  wp_remote_retrieve_body(json_decode($response_plan), true);
+
+	?><div id='rental_options' class='panel woocommerce_options_panel'><?php
+		?><div class='options_group'><?php
+
+			woocommerce_wp_select( array(
+				'label' => __('Planes Consultados', 'woocommerce'),
+				'options' => array($planes)
+				)
+			);
+		?></div>
+	</div><?php
+
+
+}
+add_action( 'woocommerce_product_data_panels', 'rental_options_product_tab_content' );
+
+
+function save_rental_option_field( $post_id ) {
+
+	$rental_option = isset( $_POST['_enable_renta_option'] ) ? 'yes' : 'no';
+	update_post_meta( $post_id, '_enable_renta_option', $rental_option );
+
+	if (!$fp = fopen("auth.txt", "r")){
+		echo "The file can't be opened";
+	}
+	$file = "auth.txt";
+	$fp = fopen($file, "r");
+	$contents = fread($fp, filesize($file));
+	fclose($fp);
+
+	$credentials = explode(" ", $contents);
+
+	$Client_Aunt = $credentials[3];
+
+	 $api_create_plan = 'https://api.payments.4geeks.io/v1/plans/create/';
+	 $dataBody = array(
+		 '-X POST',
+	     "-H authorization: bearer $Client_Aunt",
+	     '-F name=Monthly Plan',
+	     '-F amount=300000',
+	     '-F currency=crc',
+	     '-F trial_period_days=0',
+	     '-F interval=month',
+	     '-F interval_count=1', \
+	     '-F credit_card_description=Descripción'
+	);
+
+	$response_plan = wp_remote_post( $api_plan_url,
+						array(
+							'method' => 'GET',
+							'timeout' => 90,
+							'blocking' => true,
+							'headers' => array('content-type' => 'application/json'),
+							'body' => json_encode($body, true)
+						)
+								   );
+
+	if ( isset( $_POST['_text_input_y'] ) ) :
+		update_post_meta( $post_id, '_text_input_y', sanitize_text_field( $_POST['_text_input_y'] ) );
+	endif;
+
+}
+add_action( 'woocommerce_process_product_meta_simple_rental', 'save_rental_option_field'  );
+add_action( 'woocommerce_process_product_meta_variable_rental', 'save_rental_option_field'  );
+
+
+
+function hide_attributes_data_panel( $tabs) {
+
+	$tabs['attribute']['class'][] = 'hide_if_simple_rental hide_if_variable_rental';
+
+	return $tabs;
+
+}
+add_filter( 'woocommerce_product_data_tabs', 'hide_attributes_data_panel' );
 ?>
